33825531f3dde6c200a273729e8f41d8ffc4bae8
Tevinthuku <tevinthuku@gmail.com>
2021-07-13 06:35:46 +0300

    final main refactor

--- nwb.config.js
+++ nwb.config.js
@@ -9,5 +9,9 @@ module.exports = {
   },
   babel: {
     presets: ["@babel/preset-env", "@babel/preset-flow"],
+    plugins: [
+      ["@babel/plugin-proposal-private-methods", { loose: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ],
   },
 };
